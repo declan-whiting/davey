7f110539e4b8d009a02f24e69e4a4b5efdf4e382
Dec <declan-whiting@phoenixs.co.uk>
2023-05-17 15:21:26 +0100

    Reads a directory
    - Reads a directory
    - Outputs the name of each file within the directory
    - Outputs the contents of each file within the directory

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
-// Initial Commit.
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"log"
+)
+
+func main() {
+	fileSystem := os.DirFS("../testingDir")
+	
+	fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+
+
+		fmt.Println(path)
+
+		body, err := os.ReadFile("../testingDir/" + path)
+		if err != nil {
+			log.Println("unable to read file: %v", err)
+		}
+
+		fmt.Println(string(body))
+		return nil
+		})
+}
