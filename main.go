b16fceaed1f319bf182ab11f919796abfb56192c
Dec <declan-whiting@phoenixs.co.uk>
2023-05-17 16:55:55 +0100

    Write output to a file
    - Writes output to a file
    - Uses time to avoid overwriting previous runs
    - Ignore previous davey files
    - Consolidated root variable to avoid writing it out a bunch of times

--- main.go
+++ main.go
@@ -3,27 +3,49 @@ package main
 import (
 	"fmt"
 	"io/fs"
-	"os"
 	"log"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-	fileSystem := os.DirFS("../testingDir")
-	
+	var s []string
+	rootDir := "../testingDir/"
+	fileSystem := os.DirFS(rootDir)
 	fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		if path == "." || strings.Contains(path, "davey") {
+			fmt.Println(path + " is not required in the output, carrying on.")
+		} else {
+			body, err := os.ReadFile(rootDir + path)
+			if err != nil {
+				log.Println("unable to read file: %v", err)
+			}
 
-		fmt.Println(path)
-
-		body, err := os.ReadFile("../testingDir/" + path)
-		if err != nil {
-			log.Println("unable to read file: %v", err)
+			s = append(s, string(body))
 		}
 
-		fmt.Println(string(body))
 		return nil
-		})
+	})
+
+	dt := time.Now()
+	formatted := dt.Format("17052023")
+
+	f, err := os.Create(rootDir + formatted + "-davey.log")
+	if err != nil {
+		log.Println("Not able to create davey.log file")
+	}
+	defer f.Close()
+
+	for i := 0; i < len(s); i++ {
+		_, err := f.WriteString(s[i] + "\n")
+		if err != nil {
+			log.Println("unable to write")
+		}
+		fmt.Println(s[i])
+	}
 }
